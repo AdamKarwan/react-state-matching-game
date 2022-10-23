3d1a32379d6181a14eede4f8dd2407f21c200a92
Adam Karwan <akarwan@live.com>
2022-10-23 15:07:30 +1100

    not sending startgame correctly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import OptionsPanel from '../OptionsPanel'
-import Board from '../Board'
-import {createTiles } from '../../misc/utils'
+import React, { Component } from "react";
+import OptionsPanel from "../OptionsPanel";
+import Board from "../Board";
+import { createTiles } from "../../misc/utils";
 
-import './App.css';
+import "./App.css";
 
-class App extends Component{
-
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -14,25 +13,31 @@ class App extends Component{
       playing: false,
       previousTileIndex: null,
       tiles: [],
-      toBeCleared: null
-    }
+      toBeCleared: null,
+    };
   }
 
   startGame(numTiles) {
-    this.setState((state)=> ({playing:true, previousTileIndex:null, toBeCleared: null, tiles:createTiles(state.numTiles) }))
+    this.setState((state) => ({
+      playing: true,
+      previousTileIndex: null,
+      toBeCleared: null,
+      tiles: createTiles(state.numTiles),
+    }));
   }
 
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Turbo-Matcher
-      </header>
-        <OptionsPanel startGame={startGame} playing={this.state.playing} numTiles={this.state.numTiles} />
+    return (
+      <div className="App">
+        <header className="App-header">Turbo-Matcher</header>
+        <OptionsPanel
+          startGame={this.startGame}
+          playing={this.state.playing}
+          numTiles={this.state.numTiles}
+        />
         <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
-    </div>
-  );
-
+      </div>
+    );
   }
 }
 
